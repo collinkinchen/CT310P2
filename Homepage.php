68f02aeea867f79a5c580579a8897888a23e9a83
Lingyang Zhu <lyzhu@charleston.cs.colostate.edu>
2017-04-06 18:39:50 -0600

    1

--- Homepage.php
+++ Homepage.php
@@ -17,29 +17,12 @@ if (isset ( $_POST ['ingre'] )) {
 
 
 ?>
-       <!--          <div id="navbar" class="navbar-collapse collapse">
-			<ul class="nav navbar-nav">
-                            <li class="active"><a href="./Homepage.php">Home</a></li>
-                            <li><a href="">Ingredient</a></li>
-                            
-                            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" href="">Class<span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="http://www.cs.colostate.edu/~ct310">CT310</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="./aboutus.php">About Us</a></li>
-                        </ul>
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="./login.php"><span class="glyphicon glyphicon-user"></span>Login</a></li>
-                        </ul>
-                            
-			
-			</div> -->
+
 			</div>
 		</nav>
 		<div class="container-fluid">
 			<div class="row visible-on">
-	<!--		<div class="col-sm-4 col-md-4 col-lg-2">
+			<div class="col-sm-4 col-md-4 col-lg-2">
 			<div class="list-group">
 			<span class="hidden-xs">
 				<a href="./Homepage.php" class="list-group-item list-group-item-myHome">Home</a>
@@ -48,7 +31,7 @@ if (isset ( $_POST ['ingre'] )) {
                 <a href="./aboutus.php" class="list-group-item list-group-item-Kale">About Us</a>
 			</span>
 			</div>
-			</div> -->
+			</div> 
                 
                 <div class="col-xs-12 col-sm-8 col-md-8 col-lg-10">
 				
